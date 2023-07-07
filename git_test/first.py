ce866580419d17206484e0096650513590d529c7
nandani-simform <nandani@simformsolutions.com>
2023-07-07 14:08:11 +0530

    git test folder created

---
+++ git_test/first.py
@@ -0,0 +1,11 @@
+def multiply():
+    a = int(input("Enter 1st number: "))
+    b = int(input("Enter 2nd number: "))
+
+    res = a*b
+
+    print("\Multiply of 2 number:  ",res)
+    print("adding some text")
+
+
+multiply()
