8497911311fbd39c25fd31709ff70373f41985e6
nandani-simform <nandani@simformsolutions.com>
2023-07-13 18:51:34 +0530

    structure made

---
+++ scientific_calculator/script.js
@@ -0,0 +1,11 @@
+// const myDiv = document.getElementById("one");
+
+// myDiv.addEventListener('click', function() {
+//   alert("1 clicked");
+// });
+
+document.getElementById("one").addEventListener("click", myFunction);
+
+function myFunction() {
+  alert (" 1 clicked!");
+}
