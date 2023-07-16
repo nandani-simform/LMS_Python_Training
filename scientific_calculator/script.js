559795887282f078fb6a01c2fd88b9e3b73df9b9
nandani2611 <nandanidalsaniya@gmail.com>
2023-07-16 22:47:24 +0530

    function added

--- scientific_calculator/script.js
+++ scientific_calculator/script.js
@@ -1,5 +1,9 @@
-var result = ""
-var memory = 0
+let result = ""
+let memory = 0
+let answer = 0
+let doPower = false
+let lastEnteredNumber = 0
+let doMemory = 0
 
 const buttons = document.querySelectorAll('.btn')
 
@@ -10,6 +14,13 @@ Array.from(buttons).forEach((button) => {
 
         // result
         if(e.target.innerHTML == "="){
+            if(doPower){
+                result = answer + lastEnteredNumber
+                doPower = false
+            }
+            //  else {
+            //     result = eval(result)
+            // }
             result = eval(result)
             document.querySelector('input').value = result;
 
@@ -23,13 +34,23 @@ Array.from(buttons).forEach((button) => {
         // clear memory 
         else if(e.target.innerHTML == "MC"){
             memory = 0;
-
-        } 
+            document.querySelector('input').value = 0
+        }
         // save memory
         else if(e.target.innerHTML == "MS"){
-            memory = result;
-
-        } 
+            result = memory;
+            document.querySelector('input').value = result;
+        }
+        else if(e.target.innerHTML == "M+"){
+            doMemory = true
+            memory =+ lastEnteredNumber;
+            document.querySelector('input').value = lastEnteredNumber;
+        }
+        else if(e.target.innerHTML == "M-"){
+            doMemory = true
+            memory =- lastEnteredNumber;
+            document.querySelector('input').value = lastEnteredNumber;
+        }
         // square 
         else if(e.target.innerText == 'x2'){
             result = result ** 2;
@@ -66,7 +87,7 @@ Array.from(buttons).forEach((button) => {
 
         }
         // square root
-        else if(e.target.innerHTML == 'x2R'){
+        else if(e.target.closest("div").dataset.type == "root"){
             result = Math.sqrt(Number(result))
             document.querySelector('input').value = result;
         }   
@@ -91,10 +112,22 @@ Array.from(buttons).forEach((button) => {
             document.querySelector('input').value = result;
         }
         // 10**x
-        else if(e.target.innerHTML == '10P'){
+        else if(e.target.closest("div").dataset.type == '10P'){
             num = Number(result)
             result = Math.pow(10,num)
-            // result = Math.log(Number(result))
+            document.querySelector('input').value = result;
+        }
+        // x-raisedTo-y
+        else if(e.target.closest("div").dataset.type == "x-raised-to-y"){
+            // &#9744;
+            answer = Number(result) + "**"
+            result =  Number(result) + "^"
+            doPower = true
+            document.querySelector('input').value = result;
+        }
+        // pi
+        else if(e.target.closest("div").dataset.type == "pi"){
+            result = Number(result)* 3.14
             document.querySelector('input').value = result;
         }
 
@@ -105,8 +138,12 @@ Array.from(buttons).forEach((button) => {
 
 
         else {
-        // console.log(e.target)
-        result = result + e.target.innerHTML;
+        // if(doPower){
+        //     result = answer + e.target.innerHTML
+        // } else {
+            result = result + e.target.innerHTML;
+        // }
+        lastEnteredNumber = e.target.innerHTML
         document.querySelector('input').value = result;
 
 
