7df7ed5e3364080dafaf1a37181a69a995b97357
nandani-simform <nandani@simformsolutions.com>
2023-10-11 12:25:15 +0530

    total_amount function added

--- e-commerce/online_shopping/orders/models.py
+++ e-commerce/online_shopping/orders/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from user.models import CustomUser
 from shopping_cart.models import CartItem
+from django.db.models import Sum
+from decimal import Decimal
+
 
 
 class Order(models.Model):
@@ -9,6 +12,11 @@ class Order(models.Model):
     total_amount = models.DecimalField(max_digits=10, decimal_places=2) 
     shipping_address = models.CharField(max_length=200)
     cart_item = models.ForeignKey(CartItem, on_delete=models.CASCADE)
+
+
+    def calculate_total_amount(self):
+        cart_items_total = self.cart_item.aggregate(total=Sum(models.F('product__price') * models.F('quantity'), output_field=models.DecimalField()))
+        self.total_amount = cart_items_total['total'] or Decimal('0.00')
 
     def __str__(self):
         return self.order_date
--- e-commerce/online_shopping/orders/views.py
+++ e-commerce/online_shopping/orders/views.py
@@ -20,35 +20,54 @@ class CheckoutView(APIView):
         user = request.user
 
         shipping_address = request.data.get('shipping_address', '')
-        cart = CartItem.objects.filter(user=user)
-        total_amount = Decimal('0.00')
-        for items in cart:
-            product = items.product #items.product_id
-            quantity = items.quantity
-            # pid = Product.objects.get(id=product)
-            # price = pid.price
+        cart_item_ids = request.data.get('cart_item', [])
+        print(cart_item_ids)
+        print(shipping_address)
 
-            subtotal = product.price * quantity
+
+        # cart = CartItem.objects.filter(user=user)
+        # total_amount = Decimal('0.00')
+        # for items in cart:
+        #     product = items.product #items.product_id
+        #     quantity = items.quantity
+        #     # pid = Product.objects.get(id=product)
+        #     # price = pid.price
+
+        #     subtotal = product.price * quantity
+        #     total_amount += subtotal
+
+
+        total_amount = Decimal('0.00')
+        for cart_item_id in cart_item_ids:
+            cart_item = CartItem.objects.get(pk=cart_item_id)
+            subtotal = cart_item.product.price * cart_item.quantity
             total_amount += subtotal
 
+        print(total_amount, user.id)
 
-        order_data = {
-            'user': user.id, 
-            'total_amount': total_amount,
-            'shipping_address': shipping_address, 
-            'order_items': [cart_item.id for cart_item in cart]
-        }
+        # order_data = {
+        #     'user': user.id, 
+        #     'total_amount': total_amount,
+        #     'shipping_address': shipping_address, 
+        # }
 
 
         with transaction.atomic():
-            order_serializer = OrderSerializers(data=order_data)
+            order_serializer = OrderSerializers(data=request.data)
+            
             if order_serializer.is_valid():
-                order_serializer.save()
+                cart_item = order_serializer.validated_data['cart_item']
+                order_serializer.validated_data['total_amount'] = total_amount
 
-                order_instance = order_serializer.instance
-                order_instance.total_amount = total_amount
-                order_instance.save(update_fields=['total_amount'])
+                order = order_serializer.save()
                 
+
+                # order.calculate_total_amount()
+
+                # Add cart items to the order
+                # for cart_item_id in cart_item_ids:
+                #     order.cart_item.get(cart_item_id)
+
                 return Response(order_serializer.data, status=status.HTTP_201_CREATED)
             else:
                 return Response(order_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
